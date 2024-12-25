a03be3c2a9c4e3d773aa42d79092abb1f51ab98f
Shruti Sanjay Patil <patilshrutis7597@gmail.com>
2024-12-25 10:33:12 +0530

    Create AccessingVariables.py

---
+++ AccessingVariables.py
@@ -0,0 +1,12 @@
+RoomRent = 1200
+Mess = 1600
+Grocery = 500
+Lightbill = 300
+Total = (RoomRent + Mess + Grocery + Lightbill)
+item1 = "RoomRent"
+item2 = "Mess"
+item3 = "Grocery"
+item4 = "Lightbill"
+print("Expense Items = ",item1,item2,item3,item4)
+print("Monthly Expenses : ")
+print(Total)
