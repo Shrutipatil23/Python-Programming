64b30210f8b6083768309666f75e621a70b83d72
Shruti Sanjay Patil <patilshrutis7597@gmail.com>
2024-12-25 11:00:20 +0530

    Create StringSlicing.py

---
+++ StringSlicing.py
@@ -0,0 +1,9 @@
+myString = "Good Morning!"
+#Slicing From Start
+print(myString[:4])
+#Slicing To End
+print(myString[5:])
+#Slincing In Betweem
+print(myString[3:8]) 
+#Negative Slicing
+print(myString[-7 : -1])
