ff918e807ed0dac6a8e44849d766b97c027c206e
Shruti Sanjay Patil <117071349+Shrutipatil23@users.noreply.github.com>
2024-12-28 22:02:09 +0530

    Add files via upload

---
+++ ArithemeticOperator.py
@@ -0,0 +1,32 @@
+#Arithmetic Operators in Python
+
+num1 = 2
+num2 = 6
+
+#Addition Operator '+'
+AddOp = num1 + num2
+print(AddOp)
+
+#Substraction Operator '-'
+SubOp =num1 - num2
+print(SubOp)
+
+#Multiplication Operator '*'
+MulOp = num1 * num2
+print(MulOp)
+
+#Division Operator '/'
+DivOp = num2 / num1
+print(DivOp)
+
+#Modulus Operator '%'
+ModOp = num2 % num1
+print(ModOp)
+
+#Exponentation Operator '**'
+ExpOp = num1 ** num2
+print(ExpOp)
+
+#Floor Division Operator ' // '
+FloOp = num1 // num2
+print(FloOp)
