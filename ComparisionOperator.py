96dc011b6b8def14ef4751fd6b294418b55f98cf
Shruti Sanjay Patil <117071349+Shrutipatil23@users.noreply.github.com>
2024-12-29 21:14:30 +0530

    Add files via upload

---
+++ ComparisionOperator.py
@@ -0,0 +1,28 @@
+#Comparision Operator in Python
+
+num1 = 5
+num2 = 10
+
+#Equal Op
+EqualOp = (num1 == num2)
+print(EqualOp)
+
+#Not Equal
+NotEqOp = (num1 != num2)
+print(NotEqOp)
+
+#Greater Than
+GrtThanOp = (num1 > num2)
+print(GrtThanOp)
+
+#Less Than
+LessThanOp = (num1 < num2) 
+print(LessThanOp)
+
+#Greater Then Or Equal to
+GrtEqOp = (num1 >= num2)
+print(GrtEqOp)
+
+#Less Than Equal to
+LessThenEqOp = (num1 <= num2)
+print(LessThenEqOp)
